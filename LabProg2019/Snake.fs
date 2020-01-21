fa40abfab18718f1b16cca528516cafa9460fd9a
Elia <elia@bertapelle.it>
2020-01-21 09:58:27 +0100

    Aggiunta snake file

---
+++ LabProg2019/Snake.fs
@@ -0,0 +1,15 @@
+﻿(*
+* LabProg2019 - Progetto di Programmazione a.a. 2019-20
+* Maze.fs: Maze Manager
+* (C) 2020 Group 10 - Elia Bertapelle (881359), Leonardo Piccolo (882351) @ Universita' Ca' Foscari di Venezia
+*)
+
+module LabProg2019.Snake
+
+open System
+open External
+open Engine
+open Gfx
+open System.Threading
+
+let main (gm: Config.GameMod) = 0
